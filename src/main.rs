86ef6845392dfdf9c03cb5091c07b0e0d56351ca
Xethrus <xethrus@gmail.com>
2023-06-23 20:37:37 -0400

    added multiple symbol compat

--- src/main.rs
+++ src/main.rs
@@ -5,55 +5,67 @@ use statrs::statistics::Statistics;
 use std::error::Error;
 
 struct StockData {
+    symbol: String,
     mean_return: f64,
     variance: f64,
     standard_deviation: f64,
 }
 
-fn fetch_stock_symbols_data(symbol: &str, api_key: &str) -> Result<(), Box<dyn std::error::Error>>{
-    let mut easy = Easy::new();
-    let mut request_data = Vec::new();
-    let url = format!("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&apikey={}", symbol, api_key);
-    easy.url(&url)?;
-    {
-        let mut transfer = easy.transfer();
-        transfer.write_function(|new_data| {
-            request_data.extend_from_slice(new_data);
-            Ok(new_data.len())
-        })?;
-        transfer.perform()?;
+fn fetch_stock_symbols_data(symbols: &[&str], api_key: &str) -> Result<(Vec<StockData>,Vec<StockData>), Box<dyn std::error::Error>>{
+    let mut stock_data_all_group = Vec::new();
+    let mut stock_data_30_group = Vec::new();
+    for symbol in symbols {
+        let mut easy = Easy::new();
+        let mut request_data = Vec::new();
+        let url = format!("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&apikey={}", symbol, api_key);
+        easy.url(&url)?;
+        {
+            let mut transfer = easy.transfer();
+            transfer.write_function(|new_data| {
+                request_data.extend_from_slice(new_data);
+                Ok(new_data.len())
+            })?;
+            transfer.perform()?;
+        }
+        let response_body = str::from_utf8(&request_data).unwrap();
+        let data: Value = serde_json::from_str(&response_body).unwrap();
+        let timeseries = data["Time Series (Daily)"].as_object().unwrap();
+
+        let mut closing_prices_all = Vec::new();
+        let mut closing_prices_30 = Vec::new();
+
+        for (_date, values) in timeseries {
+            let close = values["4. close"].as_str().unwrap().parse::<f64>().unwrap();
+            closing_prices_all.push(close);
+        }
+
+        closing_prices_30 = closing_prices_all.clone();
+        closing_prices_30.reverse(); // reverse the vector to start from the most recent date
+        closing_prices_30.truncate(30); // limit the entries to the last 30 days
+
+        let stock_data_all = calculate_close_differences(closing_prices_all, symbol)?;
+        let stock_data_30 = calculate_close_differences(closing_prices_30, symbol)?;
+
+        let stock_all = StockData {
+            symbol: symbol.to_string(),
+            mean_return: stock_data_all.mean_return,
+            variance: stock_data_all.variance,
+            standard_deviation: stock_data_all.standard_deviation,
+        };
+        stock_data_all_group.push(stock_all);
+
+        let stock_30 = StockData {
+            symbol: symbol.to_string(),
+            mean_return: stock_data_30.mean_return,
+            variance: stock_data_30.variance,
+            standard_deviation: stock_data_30.standard_deviation,
+        };
+        stock_data_30_group.push(stock_30);
     }
-    let response_body = str::from_utf8(&request_data).unwrap();
-    let data: Value = serde_json::from_str(&response_body).unwrap();
-    let timeseries = data["Time Series (Daily)"].as_object().unwrap();
-
-    let mut closing_prices_all = Vec::new();
-    let mut closing_prices_30 = Vec::new();
-    
-    for (_date, values) in timeseries {
-        let close = values["4. close"].as_str().unwrap().parse::<f64>().unwrap();
-        closing_prices_all.push(close);
-    }
-
-    closing_prices_30 = closing_prices_all.clone();
-    closing_prices_30.reverse(); // reverse the vector to start from the most recent date
-    closing_prices_30.truncate(30); // limit the entries to the last 30 days
-
-    let stock_data_all = calculate_close_differences(closing_prices_all)?;
-    let stock_data_30 = calculate_close_differences(closing_prices_30)?;
-    
-    println!("{} mean value (all): {}",symbol, stock_data_all.mean_return);
-    println!("{} variance of value (all): {}",symbol, stock_data_all.variance);
-    println!("{} standard deviation of value (all): {}",symbol, stock_data_all.standard_deviation);
-    
-    println!("{} mean value (last 30 days): {}",symbol, stock_data_30.mean_return);
-    println!("{} variance of value (last 30 days): {}",symbol, stock_data_30.variance);
-    println!("{} standard deviation of value (last 30 days): {}",symbol, stock_data_30.standard_deviation);
-
-    Ok(())
+    Ok((stock_data_all_group, stock_data_30_group))
 }
 
-fn calculate_close_differences(closing_prices: Vec<f64>) -> Result<StockData, Box<dyn Error>> {
+fn calculate_close_differences(closing_prices: Vec<f64>, symbol: &str) -> Result<StockData, Box<dyn Error>> {
     let mut returns: Vec<f64> = Vec::new();
     for i in 0..closing_prices.len() - 1 {
         let daily_return = (closing_prices[i + 1] - closing_prices[i]) / closing_prices[i];
@@ -63,13 +75,33 @@ fn calculate_close_differences(closing_prices: Vec<f64>) -> Result<StockData, Bo
     let variance = returns.variance();
     let standard_deviation = variance.sqrt();
 
-    Ok(StockData{mean_return, variance, standard_deviation})
+    Ok(StockData{
+        symbol: symbol.to_string(),
+        mean_return,
+        variance, 
+        standard_deviation,
+    })
+
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let symbol = "IBM";
+    let symbols = vec!["IBM"];
     let api_key = "81I9AVPLTTFBVASS";
-    fetch_stock_symbols_data(symbol, api_key)?;
+    let (stock_data_all, stock_data_30) = fetch_stock_symbols_data(&symbols, api_key)?;
+    for stock in stock_data_30 {
+        println!("Stock Symbol: {}", stock.symbol);
+        println!("Mean value (last 30 days): {}", stock.mean_return);
+        println!("Variance of value (last 30 days): {}", stock.variance);
+        println!("Standard deviation of value (last 30 days): {}", stock.standard_deviation);
+        println!();
+    }
+    for stock in stock_data_all {
+        println!("Stock Symbol: {}", stock.symbol);
+        println!("Mean value (last 30 days): {}", stock.mean_return);
+        println!("Variance of value (last 30 days): {}", stock.variance);
+        println!("Standard deviation of value (last 30 days): {}", stock.standard_deviation);
+        println!();
+    }
     Ok(())
 }
 
